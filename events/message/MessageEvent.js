4b8ccc6e38e3db4741e630fbab9cff6c4f085321
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2021-08-27 19:42:14 +0300

    Update MessageEvent.js

--- events/message/MessageEvent.js
+++ events/message/MessageEvent.js
@@ -1,4 +1,4 @@
-const BaseEvent = require('../../utils/structures/BaseEvent');
+const BaseEvent = require('./utils/structures/BaseEvent');
 
 module.exports = class MessageEvent extends BaseEvent {
   constructor() {
