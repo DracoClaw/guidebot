9294693700f8c63ec597226d66bf1ca5de9ccf60
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2021-08-27 19:24:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 const { Client } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
-const config = require('../slappey.json');
+const config = require('../config.json');
 const client = new Client();
 
 (async () => {
