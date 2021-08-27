b234226e8b2420e17de495d472479f2850d3ca56
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2021-08-27 19:43:06 +0300

    Update TestCommand.js

--- commands/test/TestCommand.js
+++ commands/test/TestCommand.js
@@ -1,4 +1,4 @@
-const BaseCommand = require('../../utils/structures/BaseCommand');
+const BaseCommand = require('./utils/structures/BaseCommand');
 const Discord = require('discord.js');
 
 module.exports = class TestCommand extends BaseCommand {
