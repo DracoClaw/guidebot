8a21cf46f5a565c43622b98eafadda621597baf0
Luiz Filipe Luchesi Russo <luizluchesi@outlook.com>
2021-09-10 23:45:22 -0300

    Counting system initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { Client, Collection, Intents, Interaction, ApplicationCommandPermissionData} from "discord.js";
+import { Client, Collection, Intents, Interaction, ApplicationCommandPermissionData, Message} from "discord.js";
 import { REST } from "@discordjs/rest";
 import { GuildDefaultMessageNotifications, Routes } from 'discord-api-types/v9';
 import config = require("../config.json");
 import { CommandHandler } from "./utils/commandHandler"
 import { ICommand } from "./commands/ICommand";
+import { resolveModuleName } from "typescript";
 
 declare module "discord.js" {
     export interface Client {
@@ -11,7 +12,7 @@ declare module "discord.js" {
     }
 }
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const commandHandler = new CommandHandler(config.clientId);
 
 client.commands = new Collection();
@@ -62,6 +63,70 @@ client.on("interactionCreate", async (interaction: Interaction) => {
         const commandToExecute: ICommand = client.commands.get(commandName);
         if (commandToExecute) commandToExecute.execute(interaction);
     }
-})
+}).on("messageCreate", async (message: Message) => {
+    if (message.author.bot) return;
+
+    console.log(`Message Received!`);
+
+    if (message.channelId === config.countingChannel) {
+        await count(message)
+            .then((result) => {
+                if (result) {
+                    message.react("✅");
+                } else {
+                    message.react("🚫");
+                }
+            })
+            .catch((error) => {
+                message.reply(`Sorry <@${message.author.id}>, only numbers are allowed in this channel.`)
+                    .then((newMessage: Message) => {
+                        setTimeout(() => {
+                            newMessage.delete();
+                            message.delete();
+                        }, config.counting.textTimeout * 1000);
+                    });
+                console.error(`Not counting: ${error}`);
+            });
+    }
+});
 
 client.login(config.token);
+
+async function count(message: Message): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        try {
+            const newCount = Number(message.content);
+            if (isNaN(newCount)) {
+                reject("Not a Number!");
+                return;
+            }
+
+            if (!parseInt(message.content, 10) || newCount.toString().indexOf('.') > -1 || newCount.toString().indexOf(',') > -1) {
+                reject("Not a Integer!");
+                return;
+            }
+
+            const oldCount = config.counting.currCount;
+            console.log(`Current Count: ${oldCount}`);
+
+            if (oldCount + 1 !== newCount) {
+                message.channel.send(`Sorry <@${message.author.id}>, but that is not right. Let's start over!`);
+
+                let currHighscore = config.counting.bestCount;
+                if (currHighscore < oldCount) {
+                    config.counting.bestCount = oldCount;
+                    message.channel.send(`NEW HIGHSCORE!! We reached ${oldCount}! Let's try to surpass that!`);
+                }
+
+                config.counting.currCount = 0;
+                resolve(false);
+                return;
+            }
+
+            config.counting.currCount = newCount;
+            resolve(true);
+        } catch(error) {
+            reject(error);
+        }
+    });
+};
