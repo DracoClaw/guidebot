0e829a49aed970ac59a1b3f31cc98a6f72f4f575
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2022-02-06 02:17:52 +0200

    Boost/Patreon Embeds

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, Collection, Intents, Interaction, ApplicationCommandPermissionData, Message, GuildMember, PartialGuildMember, TextChannel} from "discord.js";
+import { Client, Collection, Intents, Interaction, ApplicationCommandPermissionData, Message, MessageEmbed, GuildMember, PartialGuildMember, TextChannel} from "discord.js";
 import { connect, getOrCreateGuildById } from "./services/database.service";
 import { count } from "./services/counting.service";
 import { assignRandomTeam } from "./services/role.service";
@@ -148,8 +148,22 @@ client.on("guildMemberUpdate", async (oldMember: GuildMember | PartialGuildMembe
 	const patreonRoleId = '730040122464141392';
     const staffChannel = client.channels.cache.get('702303566458519573') as TextChannel;
     const supporterChannel = client.channels.cache.get('753227742895407226') as TextChannel;
-
+	const generalChannel = client.channels.cache.get('697796824110465025') as TextChannel;
     const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+	
+	const patreonEmbed = new MessageEmbed()
+  .setTitle(`${oldMember.user.tag} has pledged on Patreon!`)
+  .setDescription(`The amazing ${newMember.user} has decided to support the app and pledged on GeneSy's [Patreon](https://www.patreon.com/genesy)! Thank you so much! <:emoteLove:699777339235500042>`)
+  .setColor("#F56754")
+  .setThumbnail(`${oldMember.displayAvatarURL()}`);
+	
+	const boostEmbed = new MessageEmbed()
+  .setTitle(`<:serverBoost:870144685333676102> ${oldMember.user.tag} has boosted the server! <:serverBoost:870144685333676102>`)
+  .setDescription(`The generous member ${oldMember.user} just boosted the server!!\\nLets keep it up!! <:emoteLove:699777339235500042>`)
+  .setColor("#F47FFF")
+  .setFooter("After 7 days of boosting, your team will be awarded 50 points.")
+  .setThumbnail(`${oldMember.displayAvatarURL()}`);
+	
     if (removedRoles.size > 0) {
         // channel?.send(`Role${removedRoles.size > 1 ? 's' : ''} ${removedRoles.map(role => role.name).join(", ")} removed from ${oldMember.displayName}!`);
         console.log(`Role${removedRoles.size > 1 ? 's' : ''} ${removedRoles.map(role => role.name).join(", ")} removed from ${oldMember.displayName}!`);
@@ -170,12 +184,14 @@ client.on("guildMemberUpdate", async (oldMember: GuildMember | PartialGuildMembe
    if (addedRoles.hasAny(patreonRoleId)) {
         staffChannel?.send(`New Patreon Supporter: ${oldMember.user}!`);
         supporterChannel?.send(`Welcome ${oldMember.user}! Thank you for suppoting GeneSy on Patreon. <:emoteLove:699777339235500042>`);
+		generalChannel?.send({embeds: [patreonEmbed]}).catch(console.error);
         console.log(`New member: ${oldMember.user.tag}!`);
     }
 	
     if (newMember.premiumSinceTimestamp && newMember.premiumSinceTimestamp !== oldMember.premiumSinceTimestamp) {
         staffChannel?.send(`New Server Booster: ${oldMember.user}!`);
         supporterChannel?.send(`Welcome ${oldMember.user}! Thank you for Boosting the server. <:emoteLove:699777339235500042>`);
+		generalChannel?.send({embeds: [boostEmbed]}).catch(console.error);
         console.log(`New member: ${oldMember.user.tag}!`);    }
 });
 
