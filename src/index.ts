9c5b8c4b53c27c0778ac624d63269904731076bb
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2022-05-01 15:16:12 +0300

    fix setFooter depricated

--- src/index.ts
+++ src/index.ts
@@ -169,19 +169,6 @@ client.on("guildMemberUpdate", async (oldMember: GuildMember | PartialGuildMembe
     const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
 	
 	
-	const patreonEmbed = new MessageEmbed()
-  .setTitle(`${oldMember.user.tag} has pledged on Patreon!`)
-  .setDescription(`The amazing ${newMember.user} has decided to support the app and pledged on GeneSy's [Patreon](https://www.patreon.com/genesy)! Thank you so much! <:emoteLove:699777339235500042>`)
-  .setColor("#F56754")
-  .setThumbnail(`${oldMember.displayAvatarURL()}`);
-	
-	const boostEmbed = new MessageEmbed()
-  .setTitle(`<:serverBoost:870144685333676102> ${oldMember.user.tag} has boosted the server! <:serverBoost:870144685333676102>`)
-  .setDescription(`The generous member ${oldMember.user} just boosted the server!!\nLets keep it up!! <:emoteLove:699777339235500042>`)
-  .setColor("#F47FFF")
-  .setFooter("After 7 days of boosting, your team will be awarded 50 points.")
-  .setThumbnail(`${oldMember.displayAvatarURL()}`);
-	
     if (removedRoles.size > 0) {
         // channel?.send(`Role${removedRoles.size > 1 ? 's' : ''} ${removedRoles.map(role => role.name).join(", ")} removed from ${oldMember.displayName}!`);
         console.log(`${guild.guildId} | Role${removedRoles.size > 1 ? 's' : ''} ${removedRoles.map(role => role.name).join(", ")} removed from ${oldMember.displayName}!`);
@@ -236,17 +223,40 @@ client.on("guildMemberUpdate", async (oldMember: GuildMember | PartialGuildMembe
     }
 	
    if (addedRoles.hasAny(patreonRoleId)) {
+        if (newMember.guild.id != "695630972082978986") return;
         staffChannel?.send(`New Patreon Supporter: ${oldMember.user}!`);
         supporterChannel?.send(`Welcome ${oldMember.user}! Thank you for suppoting GeneSy on Patreon. <:emoteLove:699777339235500042>`);
-		generalChannel?.send({embeds: [patreonEmbed]}).catch(console.error);
+		generalChannel?.send({embeds: [{
+            "title": `${oldMember.user.tag} has pledged on Patreon!`,
+            "color": 16082772,
+            "description": `The amazing ${newMember.user} has decided to support the app and pledged on GeneSy's [Patreon](https://www.patreon.com/genesy)! Thank you so much! <:emoteLove:699777339235500042>`,
+            "thumbnail": {
+              "url": `${oldMember.displayAvatarURL()}`
+            },
+            "footer": {
+              "text": "After 7 days of boosting, your team will be awarded 50 points."
+            }
+          }]}).catch(console.error);
         console.log(`${guild.guildId} | New member: ${oldMember.user.tag}!`);
     }
 	
     if (newMember.premiumSinceTimestamp && newMember.premiumSinceTimestamp !== oldMember.premiumSinceTimestamp) {
+        if (newMember.guild.id != "695630972082978986") return;
         staffChannel?.send(`New Server Booster: ${oldMember.user}! Don't forget to give them points in 7 days.`);
         supporterChannel?.send(`Welcome ${oldMember.user}! Thank you for Boosting the server. <:emoteLove:699777339235500042>`);
-		generalChannel?.send({embeds: [boostEmbed]}).catch(console.error);
-        console.log(`${guild.guildId} | New member: ${oldMember.user.tag}!`);    }
+		generalChannel?.send({embeds: [{
+            "title": `<:serverBoost:870144685333676102> ${oldMember.user.tag} has boosted the server! <:serverBoost:870144685333676102>`,
+            "color": 16023551,
+            "description": `The generous member ${oldMember.user} just boosted the server!!\nLets keep it up!! <:emoteLove:699777339235500042>`,
+            "thumbnail": {
+              "url": `${oldMember.displayAvatarURL()}`
+            },
+            "footer": {
+              "text": "After 7 days of boosting, your team will be awarded 50 points."
+            }
+          }]}).catch(console.error);
+        console.log(`${guild.guildId} | New member: ${oldMember.user.tag}!`);
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
